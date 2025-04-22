e5b0eb8699f647a21b5ba315e2ae00539a8b8c7d
popadina varvara <popadinavar@ya.ru>
2025-04-22 21:37:05 +0300

    added an icon and adjusted the styles

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,23 +1,17 @@
 import "@/styles"
 
-import { Head } from "minista"
 import Header from "@/layouts/Header"
 import Content from "@/layouts/Content"
+
+export const metadata = {
+  title: "Главная",
+}
 
 export default function () {
   return (
     <>
-      <Head htmlAttributes={{ lang: "ru" }}>
-        <title>Поздравлю | Главная страница</title>
-        <script src="/src/main.js" type="module"></script>
-      </Head>
       <Header />
-      <Content>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ullam
-        aliquam? Earum voluptate, quis architecto, obcaecati voluptas sit quia,
-        possimus nisi dicta aliquam nihil velit! Alias tempore vero voluptate
-        vel.
-      </Content>
+      <Content></Content>
     </>
   )
 }
